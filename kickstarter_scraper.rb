53a311e545402172be5fd3ab5ee4c1913f712981
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-02 11:58:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,10 +11,10 @@ def create_project_hash
     title = project.css('h2.bbcard_name strong a').text
 
     projects[title.to_sym] = {
-      image_link: project.css('img.projectphoto-little').attribute('src'),
-      location: project.css('span.location-name').text,
-      description: project.css('p.bbcard_blurb').text,
-      percent_funded: project.css('li.funded').text
+      :image_link => project.css('img.projectphoto-little').attribute('src'),
+      :location => project.css('span.location-name').text,
+      :description => project.css('p.bbcard_blurb').text,
+      :percent_funded => project.css('li.funded').text
     }
 
 
