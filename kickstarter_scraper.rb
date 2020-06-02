a5236faf0ea24f496cff2062347d8db3008f48e7
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-02 11:45:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,5 +5,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  puts kickstarter
+  projects = kickstarter.css('.project grid_4')
+
+  puts projects
 end
