04edfa7a1587fd1a54208b03dd95e3ef161d8b4d
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-02 11:56:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,9 +6,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  projects = kickstarter.css('li.project')
+  kickstarter.css('li.project').each do |project|
 
-  projects.each do |project|
     # title, an image, a short description, a location and some funding details
     puts title = project.css('h2.bbcard_name strong a').text
     puts image_link = project.css('img.projectphoto-little').attribute('src')
